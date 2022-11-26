b0a5cb22e26101f50b3e29c01fa77cf6db5d6ba5
takishun <shun.takinami@gmail.com>
2022-11-27 02:48:02 +0900

    Add files via upload

--- Pokemon.py
+++ Pokemon.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
                     st.text('歯茎を見せたら止まらない。')
 
             if option == 'エタ':
-                values = np.array([31, 50, 22, 32, 99])
+                values = np.array([50, 50, 30, 77, 10])
                 labels = ["知力","判断力","育成力","メンタル","運"]
                 col1, col2 = st.columns([1,1])
 
@@ -132,6 +132,49 @@ if __name__ == "__main__":
                     st.pyplot(fig)
                 with col2:
                     st.text('企画でいっぱいいっぱいw')
+                    
+            if option == 'カイト':
+                values = np.array([60, 75, 45, 60, 50])
+                labels = ["知力","判断力","育成力","メンタル","運"]
+                col1, col2 = st.columns([1,1])
+
+                with col1:
+                    # 多角形を閉じるためにデータの最後に最初の値を追加する。
+                    radar_values = np.concatenate([values, [values[0]]])
+                    # プロットする角度を生成する。
+                    angles = np.linspace(0, 2 * np.pi, len(labels) + 1, endpoint=True)
+                    # メモリ軸の生成
+                    rgrids = [0, 20, 40, 60, 80, 100]
+                    fig = plt.figure(facecolor="w")
+                    # 極座標でaxを作成
+                    ax = fig.add_subplot(1, 1, 1, polar=True)
+                    # レーダーチャートの線を引く
+                    ax.plot(angles, radar_values)
+                    #　レーダーチャートの内側を塗りつぶす
+                    ax.fill(angles, radar_values, alpha=0.2)
+                    # 項目ラベルの表示
+                    ax.set_thetagrids(angles[:-1] * 180 / np.pi, labels)
+                    # 円形の目盛線を消す
+                    ax.set_rgrids([])
+                    # 一番外側の円を消す
+                    ax.spines['polar'].set_visible(False)
+                    # 始点を上(北)に変更
+                    ax.set_theta_zero_location("N")
+                    # 時計回りに変更(デフォルトの逆回り)
+                    ax.set_theta_direction(-1)
+                    # 多角形の目盛線を引く
+                    for grid_value in rgrids:
+                        grid_values = [grid_value] * (len(labels)+1)
+                        ax.plot(angles, grid_values, color="gray",  linewidth=0.5)
+                    # メモリの値を表示する
+                    for t in rgrids:
+                        # xが偏角、yが絶対値でテキストの表示場所が指定される
+                        ax.text(x=0, y=t, s=t)
+                    ax.set_rlim([min(rgrids), max(rgrids)])
+                    ax.set_title("トレーナースキル", pad=20)
+                    st.pyplot(fig)
+                with col2:
+                    st.text('見た目はカビゴン')
             
         with tab4:
             st.header('ルール')
